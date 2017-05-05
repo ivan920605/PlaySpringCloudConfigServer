5076d82ba0052ba025cc0d3bde8e94f003d7d4ce
Liangyi Chen <liangyi.chen@pwc.com>
2017-05-05 11:45:18 +0800

    spring cloud client app

--- springcloud.configserver/src/main/java/ivan/play/springcloud/configserver/ConfigServiceApplication.java
+++ springcloud.configserver/src/main/java/ivan/play/springcloud/configserver/ConfigServiceApplication.java
@@ -1,8 +1,11 @@
 package ivan.play.springcloud.configserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Hello Spring Cloud Config Server!
@@ -12,11 +15,25 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
+@RestController
 public class ConfigServiceApplication
 {
+	@Value("${application.name}")
+	private String applicationName;
+
+	@Value("${application.description}")
+	private String applicationDescription;
+
 	public static void main(String[] args)
 	{
 		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+	
+	
+	@RequestMapping("/hello")
+	public String HelloSpringCloudServerApp()
+	{
+		return applicationDescription;
 	}
 
 }
