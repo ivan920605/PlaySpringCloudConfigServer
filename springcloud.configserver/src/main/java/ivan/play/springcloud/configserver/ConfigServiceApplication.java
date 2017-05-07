697c1fd7e5932037f921a7803b126acaf4302f03
Ivan <liangyichen828@yahoo.com>
2017-05-07 22:39:25 +0800

    change application properties

--- springcloud.configserver/src/main/java/ivan/play/springcloud/configserver/ConfigServiceApplication.java
+++ springcloud.configserver/src/main/java/ivan/play/springcloud/configserver/ConfigServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableConfigServer
 @SpringBootApplication
 @RestController
-@EnableEurekaClient
+/* @EnableEurekaClient */
 public class ConfigServiceApplication
 {
 	@Value("${spring.application.name}")
